ab098a4f8ad4283809c23089c61fedfbf59ab6e9
TFKSM <43906900+TFKSM@users.noreply.github.com>
2020-02-21 23:19:56 +0900

    TEST

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def handle_message(event):
     
     line_bot_api.reply_message(
        event.reply_token,
-       TextSendMessage(text=messages))
+       TextSendMessage("みたで！"))
     
     line_bot_api.reply_message(
         event.reply_token,
